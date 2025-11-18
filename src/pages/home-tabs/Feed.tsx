60e515a49e3378c8ab1c485c19cacd9f01f90799
Annalinneee <20221154@nbsc.edu.ph>
2025-11-19 07:54:46 +0800

    Add local Ionic project files

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -43,47 +43,7 @@ const Feed: React.FC = () => {
 
       <IonContent fullscreen className="ion-padding">
 
-        <IonCard>
-          <img
-            alt="Silhouette of mountains"
-            src="https://ionicframework.com/docs/img/demos/card-media.png"
-          />
-          <IonCardHeader>
-            <IonCardTitle>ONE PIECE: FILM RED</IonCardTitle>
-          </IonCardHeader>
-
-          <IonCardContent>
-          Uta, the most beloved singer on the planet, is known for hiding her own identity when performing. Now, for the first time, she will reveal herself to the world. All of Uta's fans, including the Straw Hats led by Luffy, await.
-          </IonCardContent>
-        </IonCard>
-
-        <IonCard>
-          <img
-            alt="Silhouette of mountains"
-            src="https://ionicframework.com/docs/img/demos/card-media.png"
-          />
-          <IonCardHeader>
-            <IonCardTitle>ONE PIECE: DRESSROSA ARC</IonCardTitle>
-          </IonCardHeader>
-
-          <IonCardContent>
-          The Straw Hats and Law reach Dressrosa where they plan to bring down Doflamingo. A sequence of unexpected events shakes the world order, and Doflamingo proves to be a challenge that even some of the strongest warriors cannot overcome.
-          </IonCardContent>
-        </IonCard>
-
-        <IonCard>
-          <img
-            alt="Silhouette of mountains"
-            src="https://ionicframework.com/docs/img/demos/card-media.png"
-          />
-          <IonCardHeader>
-            <IonCardTitle>ONE PIECE: EGGHEAD ARC</IonCardTitle>
-          </IonCardHeader>
-
-          <IonCardContent>
-          Luffy arrived on the island of the future, Egghead, where he met Vegapunk. Given that Egghead followed the future theme, fans knew that this would be an arc focused on technology, as well as many inventions made by Vegapunk over the years.
-          </IonCardContent>
-        </IonCard>
+        
 
         {/* Floating Action Button */}
         <IonFab vertical="bottom" horizontal="end" slot="fixed">
