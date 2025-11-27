3f059f6c931bf868938b468ffc28fd55dcc7e3fe
ANNBELLA2006 <annabellasuposo2006@gmail.com>
2025-11-27 13:02:06 +0800

    Updates

--- assets/ios.transition-legacy-CXgvlcGr.js
+++ assets/ios.transition-legacy-CXgvlcGr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bw2OxZ3y.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Duc3zXct.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-Dl_exxls.js
+++ assets/swipe-back-legacy-Dl_exxls.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bw2OxZ3y.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Duc3zXct.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
